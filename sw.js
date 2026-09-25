ce077a028356ecaa618c21cde9f05bf4bf6cbbb3
vishalgojha <vishalgojha@users.noreply.github.com>
2026-09-25 17:31:28 +0530

    Make Sur Guru installable on mobile

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'sur-guru-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './worklet/dsp.js',
+  './worklet/pitch-processor.js',
+  './worklet/recorder-processor.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
